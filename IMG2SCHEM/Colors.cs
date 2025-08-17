03c576edbfbf8704c757ceeb382ce55aaede5447
Arda Eren <Poedx2@gmail.com>
2025-08-17 04:48:51 +0300

    Refactor CLI options and add Colors struct for image processing
    Introduces a new Colors.cs file with Color and ColorBlock structs to encapsulate color data and operations. Refactors Program.cs to improve CLI options handling, validation, and output logic, including support for image resolution, color amount, dithering, and output destinations. Adds debug and verbose output, improves user prompts, and updates launchSettings.json to include the clipboard output option.

---
+++ IMG2SCHEM/Colors.cs
@@ -0,0 +1,80 @@
+﻿using ImageMagick;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMG2SCHEM
+{
+
+    struct ColorBlock
+    {
+        public int x, y, width, height;
+        public Color col;
+
+        public ColorBlock(int x, int y, int width, int height, Color col)
+        {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+            this.col = col;
+        }
+
+        public override string ToString()
+        {
+            return $"{x}, {y} : {width}, {height}; {col.ToString()}";
+        }
+    }
+    struct Color
+    {
+        public byte r, g, b, a;
+
+        public Color(byte r, byte g, byte b, byte a)
+        {
+            this.r = r;
+            this.g = g;
+            this.b = b;
+            this.a = a;
+        }
+
+        public static bool operator ==(Color col1, Color col2)
+        {
+            if (
+                col1.r == col2.r &&
+                col1.g == col2.g &&
+                col1.b == col2.b &&
+                col1.a == col2.a
+                )
+                return true;
+            return false;
+        }
+        public static bool operator !=(Color col1, Color col2)
+        {
+            if (
+                col1.r == col2.r &&
+                col1.g == col2.g &&
+                col1.b == col2.b &&
+                col1.a == col2.a
+                )
+                return false;
+            return true;
+        }
+
+        public static Color FromArray(IPixel<byte> arr, uint channels)
+        {
+            byte r = arr[0];
+            byte g = arr[1];
+            byte b = arr[2];
+            byte a = 255;
+
+            return new Color(r, g, b, a);
+        }
+
+        public override string ToString()
+        {
+            return $"{r},{g},{b},{a}";
+        }
+    }
+}
--- IMG2SCHEM/Program.cs
+++ IMG2SCHEM/Program.cs
@@ -1,4 +1,5 @@
 ﻿using CommandLine;
+using CommandLine.Text;
 using ImageMagick;
 using ImageMagick.Colors;
 using MindustryChematicCreator;
@@ -7,6 +8,7 @@ using MindustrySchematicCreator;
 using System.Diagnostics;
 using System.Drawing;
 using System.IO.Pipelines;
+using System.Net.Security;
 using System.Text;
 using System.Threading.Channels;
 using TextCopy;
@@ -17,112 +19,169 @@ namespace IMG2SCHEM
     {
         class Options
         {
-            [Option('i', "input", Required = true, HelpText = "Input file to be processed.")]
-            public string InputFile { get; set; }
+            #region file io
+
+            [Option('i', "input", Required = true, HelpText = "Input image to be processed.")]
+            public string InputFile { get; set; } = "";
+
             [Option('o', "output", Required = false, HelpText = "Output path to place the schematic base64.")]
-            public string OutputFile { get; set; }
+            public string? OutputFile { get; set; }
+            public bool OutputToFile => !string.IsNullOrWhiteSpace(OutputFile);
+
+            #endregion
+
+            #region image settings
+
+            [Option(shortName: 'r', longName: "image-resolution", Required = false, HelpText = "The resolution to resize the input image to.", Default = (uint)176)]
+            public uint ImageResolution { get; set; }
+
+            [Option(shortName: 'c', longName: "color-amount", Required = false, HelpText = "The amount of colors to be used.", Default = (uint)32)]
+            public uint ColorAmount { get; set; }
+
+            [Option(shortName: 'd', longName: "dittering-method", Required = false, HelpText = "The dittering method to be used", Default = DitherMethod.No)]
+            public DitherMethod ImageDitherMethod { get; set; }
+
+            #endregion
+
+            #region debug settings
 
             [Option('v', "verbose", Required = false, HelpText = "Detailed outputs.")]
             public bool Verbose { get; set; }
-        }
-        struct ColorBlock
-        {
-            public int x, y, width, height;
-            public Color col;
 
-            public ColorBlock(int x, int y, int width, int height, Color col)
-            {
-                this.x = x;
-                this.y = y;
-                this.width = width;
-                this.height = height;
-                this.col = col;
-            }
+            [Option("debug", Required = false, HelpText = "Debug mode.")]
+            public bool Debug { get; set; } = false;
 
-            public override string ToString()
-            {
-                return $"{x}, {y} : {width}, {height}; {col.ToString()}";
-            }
-        }
-        struct Color
-        {
-            public byte r, g, b, a;
+            #endregion
 
-            public Color(byte r, byte g, byte b, byte a)
-            {
-                this.r = r;
-                this.g = g;
-                this.b = b;
-                this.a = a;
-            }
+            #region y/n
 
-            public static bool operator ==(Color col1, Color col2)
-            {
-                if (
-                    col1.r == col2.r &&
-                    col1.g == col2.g &&
-                    col1.b == col2.b &&
-                    col1.a == col2.a
-                    )
-                    return true;
-                return false;
-            }
-            public static bool operator !=(Color col1, Color col2)
-            {
-                if (
-                    col1.r == col2.r &&
-                    col1.g == col2.g &&
-                    col1.b == col2.b &&
-                    col1.a == col2.a
-                    )
-                    return false;
-                return true;
-            }
+            [Option('y', "yes", Required = false, HelpText = "Override output files.")]
+            public bool OverrideExistingFile { get; set; } = false;
 
-            public static Color FromArray(IPixel<byte> arr, uint channels)
-            {
-                byte r = arr[0];
-                byte g = arr[1];
-                byte b = arr[2];
-                byte a = 255;
+            [Option('n', "no", Required = false, HelpText = "Never override output files.")]
+            public bool DoNotOverrideExistingFile { get; set; } = false;
 
-                return new Color(r, g, b, a);
-            }
+            #endregion
 
-            public override string ToString()
-            {
-                return $"{r},{g},{b},{a}";
-            }
+            #region output settings
+
+            [Option('p', "clipboard", Required = false, HelpText = "Pastes the output schematic to clipboard.", Default = false)]
+            public bool PasteToClipBoard { get; set; }
+
+            [Option("print-output", Required = false, HelpText = "Print output schematic Base64 to console.")]
+            public bool PrintOutput { get; set; } = false;
+
+            #endregion
+
+            #region schematic settings
+
+            [Option("name", Required = false, HelpText = "Name of the output schematic.", Default = "Unnamed")]
+            public string SchematicName { get; set; } = "Unnamed";
+
+            #endregion
         }
 
         static void Main(string[] args)
         {
-            Options options = Parser.Default.ParseArguments<Options>(args).WithNotParsed<Options>(opts => { throw new ArgumentNullException("CLI Arguments not filled"); }).Value;
+            var parser = new Parser(with =>
+            {
+                with.CaseInsensitiveEnumValues = true;
+                with.AutoHelp = true;
+                with.AutoVersion = true;
+            });
 
+            parser.ParseArguments<Options>(args)
+                .WithParsed(options =>
+                {
+                    if (AreOptionsValid(options))
+                        Run(options);
+
+
+                })
+                .WithNotParsed(errors =>
+                {
+                    var helpText = HelpText.AutoBuild
+                    (
+                            parser.ParseArguments<Options>(args),
+                            h =>
+                            {
+                                h.AutoVersion = true;
+                                h.AutoHelp = true;
+                                h.AddEnumValuesToHelpText = true;
+                                return h;
+                            }
+                    );
+                    Console.WriteLine(helpText);
+                });
+        }
+        static bool AreOptionsValid(Options options)
+        {
+            if (options.OverrideExistingFile && options.DoNotOverrideExistingFile)
+            {
+                Console.WriteLine("Error: Cannot use both -y and -n options together.");
+                return false;
+            }
+            if (!File.Exists(options.InputFile))
+            {
+                Console.WriteLine($"Error: '{options.InputFile}' cannot be found.");
+                return false;
+            }
+            if (options.OutputToFile && !Path.IsPathRooted(options.OutputFile))
+            {
+                Console.WriteLine($"Error: '{options.OutputFile}' is not a valid output path.");
+                return false;
+            }
+            if (options.ImageResolution > 176)
+            {
+                Console.WriteLine("Error: Image resolution cannot exceed 176.");
+                return false;
+
+            }
+            if (options.ColorAmount is < 2 or > 256)
+            {
+                Console.WriteLine("Error: Color amount must be between 2 and 256.");
+                return false;
+            }
+
+            if (!options.OutputToFile && !options.PasteToClipBoard && !options.PrintOutput)
+            {
+                if (PromptYesNo("No output specified. Save to file?"))
+                    options.OutputFile = Path.ChangeExtension(options.InputFile, ".schem");
+                else
+                    return false;
+            }
+            return true;
+        }
+        static void Run(Options options)
+        {
             MagickImage image = new MagickImage(File.ReadAllBytes(options.InputFile));
             image.Flip();
 
-            // 176
-            (uint imgResizeWidth, uint imgResizeHeight) = (176, 176);
-            (float scaleRatioX, float scaleRatioY) = (176f / imgResizeWidth, 176f / imgResizeHeight);
+            uint imgRes = options.ImageResolution;
 
-            MagickGeometry geometry = new(imgResizeWidth, imgResizeHeight);
+            (float scaleRatioX, float scaleRatioY) = (176f / imgRes, 176f / imgRes);
+
+            MagickGeometry geometry = new(imgRes, imgRes);
             geometry.IgnoreAspectRatio = true;
             image.Resize(geometry);
 
             QuantizeSettings settings = new();
 
-            settings.Colors = 32;
-            settings.DitherMethod = DitherMethod.FloydSteinberg;
+            settings.Colors = options.ColorAmount;
+            settings.DitherMethod = options.ImageDitherMethod;
 
             image.Quantize(settings);
 
-            image.WriteAsync(new FileInfo(@"D:\Github\IMG2SCHEM\IMG2SCHEM\out.jpg"));
+            if (options.Debug)
+            {
+                string debugImageOutPath = Path.GetTempPath() + Guid.NewGuid().ToString() + ".jpg";
+                image.Write(new FileInfo(debugImageOutPath));
+                Console.WriteLine("Wrote proccessed image into: " + debugImageOutPath);
+            }
 
 
-
+            #region color block creation
             IPixelCollection<byte> pixels = image.GetPixels();
-
 
             uint width = image.Width;
             uint height = image.Height;
@@ -176,10 +235,11 @@ namespace IMG2SCHEM
                     blocks.Add(new ColorBlock(x, y, runW, runH, pixelCol));
                 }
             }
-
+            #endregion
 
 
             Schematic schem = new();
+            schem.Name = options.SchematicName;
 
             Block display = new(BlockData.Blocks[BlockType.large_logic_display], 1, 0);
             schem.AddBlock(display);
@@ -194,17 +254,16 @@ namespace IMG2SCHEM
                 return colorA.CompareTo(colorB);
             });
 
+
             Color? lastColor = blocks[0].col;
             int blockOffset = 0;
             int newlineCount = 0;
-            float waitTime = 9f;
             string outCode = "";
+
+            #region code creation functions
             // adds the flush command
             void flush()
             { sb.AppendLine($"drawflush display1"); newlineCount++; }
-            // adds the stop command
-            void stop()
-            { sb.AppendLine($"stop"); newlineCount++; }
 
             // adds the logic block to the schematic
             void addLogic()
@@ -225,15 +284,15 @@ namespace IMG2SCHEM
                 sb.Clear(); // clear the code for the next commands
                 newlineCount = 0; // reset the line counter
             }
+            #endregion
 
+            #region code creation
             for (int blockIndex = 0; blockIndex < blocks.Count; blockIndex++)
             {
                 ColorBlock drawBlock = blocks[blockIndex];
 
                 void drawCol()
                 { sb.AppendLine($"draw color {drawBlock.col.r} {drawBlock.col.g} {drawBlock.col.b} {drawBlock.col.a} 0 0"); newlineCount++; }
-                void wait()
-                { sb.AppendLine($"wait {waitTime}"); newlineCount++; }
 
                 if (lastColor != drawBlock.col || !sb.ToString().Contains("draw color"))
                 {
@@ -253,58 +312,110 @@ namespace IMG2SCHEM
 
 
                 if (newlineCount % 100 == 0)
+                {
                     flush();
+                    drawCol();
+                }
 
                 // filter so we dont pass the insturction limit
                 if (newlineCount >= 995)
                 {
                     flush(); // draw everything left
-                    stop();  // stop the logic from continuing work
 
                     // get all the code
                     clear(); // clear the sb and update the code string
                     addLogic(); // add the logic block
-
-                    wait(); // add wait so the next drawing happens when this one finishes
-                    waitTime += 9f; // increase the wait time for each block so they dont conflict 
 
                     drawCol(); // add the draw color as this is the new block
 
                     blockOffset++; // Move to the next processor slot (vertical offset in schematic)
                 }
             }
+            #endregion
 
+            // be sure to finish up the last logic block
             flush();
-            stop();
             clear();
             addLogic();
 
 
-            List<Color> usedColors = new List<Color>();
+            string schemOut = schem.GetBase64();
 
-            lastColor = null;
-            foreach (var block in blocks)
+            if (options.OutputToFile)
             {
-                if (lastColor == null || lastColor != block.col)
+                if (File.Exists(options.OutputFile))
                 {
-                    usedColors.Add(block.col);
-                    lastColor = block.col;
+                    if (options.DoNotOverrideExistingFile)
+                        Console.WriteLine($"File '{options.OutputFile}' already exists.");
+                    else if (options.OverrideExistingFile)
+                        File.WriteAllText(options.OutputFile, schemOut);
+                    else if (PromptYesNo($"File '{options.OutputFile}' already exists. Override?"))
+                        File.WriteAllText(options.OutputFile, schemOut);
                 }
+                else
+                    File.WriteAllText(options.OutputFile!, schemOut);
             }
 
-            string schemOut = schem.GetBase64();
-            ClipboardService.SetText(schemOut);
-            Console.WriteLine();
-            Console.WriteLine(blocks.Count);
-            Console.WriteLine(schem.Blocks.Count);
-            usedColors.ForEach(s => Console.WriteLine(s.ToString()));
+            if (options.PasteToClipBoard)
+                try { ClipboardService.SetText(schemOut); }
+                catch (Exception ex) { Console.WriteLine($"Clipboard not supported: {ex.Message}"); }
+            if (options.PrintOutput)
+                Console.WriteLine(schemOut);
 
-            Schematic k = Schematic.FromBase64("bXNjaAF4nGPgYGBnZmDJS8xNZWApSc0tYOBOSS1OLsosKMnMz2NgYGDLSUxKzSlmYIqOZWJgTinNZ+DPzUwuytctKMpPTi0uzi8CKmJmgABGBnYGVj4gQ6piTnJKA4Mb27qmRX+eXHn06I+Ss4A8g0YFizxYERtIETdYERMDgxADEwMAKU4g6w==");
-
-            string s = "";
-
-
+            ConfirmOutput(options, schem, blocks);
         }
+        static void ConfirmOutput(Options options, Schematic schem, List<ColorBlock> blocks)
+        {
+            StringBuilder output = new();
 
+            output.AppendLine($"Successfully created schematic: {options.SchematicName}");
+            output.AppendLine($"Input: {options.InputFile}");
+            output.AppendLine($"Output resolution: {options.ImageResolution}x{options.ImageResolution}");
+            output.AppendLine($"Color amount: {options.ColorAmount}");
+            output.AppendLine($"Proccessors used: {schem.Blocks.Where(s => s.data.Name.Contains("processor")).ToArray().Length}");
+            output.AppendLine($"Total blocks: {schem.Blocks.Count}");
+            if (options.OutputToFile)
+                output.AppendLine($"Output: {options.OutputFile}");
+            if (options.PasteToClipBoard)
+                output.AppendLine($"Successfully pasted to clipboard.");
+            if (options.Verbose)
+            {
+                output.AppendLine("v:");
+                output.AppendLine("\t" + "Color block count: " + blocks.Count);
+                output.AppendLine("\t" + "Used colors:");
+                #region used colors creation
+                List<Color> usedColors = new List<Color>();
+                Color? lastColor = null;
+                foreach (var block in blocks)
+                {
+                    if (lastColor == null || lastColor != block.col)
+                    {
+                        usedColors.Add(block.col);
+                        lastColor = block.col;
+                    }
+                }
+                #endregion
+                usedColors.ForEach(c => output.AppendLine($"\t\t#{c.r:X2}{c.g:X2}{c.b:X2}{c.a:X2}"));
+            }
+
+            Console.WriteLine(output.ToString());
+        }
+        static bool PromptYesNo(string message)
+        {
+            while (true)
+            {
+                Console.Write($"{message} (y/n): ");
+                ConsoleKey key = Console.ReadKey(intercept: true).Key;
+                Console.WriteLine();
+
+                switch (key)
+                {
+                    case ConsoleKey.Y: return true;
+                    case ConsoleKey.N: return false;
+                    default: continue;
+                }
+            ;
+            }
+        }
     }
 }
